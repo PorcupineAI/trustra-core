55d3cffa467e68cd8304383bc14a16e76d01c3cc
PorcupineAI <okerekevip@gmail.com>
2025-12-25 00:50:01 +0100

    Delete backend/app/config.py

--- backend/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
-PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
-VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
-
-PAYSTACK_SECRET = os.getenv("PAYSTACK_SECRET")
-BASE_URL = "https://graph.facebook.com/v19.0"
