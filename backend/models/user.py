b3cda6b8d66143b47734d263632a464f4332138b
PorcupineAI <okerekevip@gmail.com>
2025-12-25 02:04:08 +0100

    Delete backend/models directory

--- backend/models/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, phone, role):
-        self.phone = phone
-        self.role = role  # buyer | seller
-        self.verified = False
-        self.trust_badge = False
-        self.paid = False
