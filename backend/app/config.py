98658e22942483983750f83967e907dda5019961
PorcupineAI <okerekevip@gmail.com>
2025-12-29 01:42:55 +0100

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,6 @@
 import os
 
-NEON_DB_URL = os.getenv("NEON_DB_URL", "postgresql://username:password@host:port/dbname")
+DATABASE_URL = os.getenv("DATABASE_URL")
 PAYSTACK_SECRET_KEY = os.getenv("PAYSTACK_SECRET_KEY")
 WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "trustra_verify")
